89573f18c5b4c871c63f9258f9458de7cbdbae59
Taylor Keller <takeller17@gmail.com>
2020-07-14 09:58:27 -0600

    Adds detailed info spec

--- spec/features/visitor_can_search_for_fire_nation_members_spec.rb
+++ spec/features/visitor_can_search_for_fire_nation_members_spec.rb
@@ -11,5 +11,21 @@ describe 'As a visitor' do
       expect(current_path). to eq('/search')
       expect(page).to have_css('.member', count: 20)
     end
+
+    it 'I see detailed information for each member' do
+      visit '/'
+
+      select 'Fire Nation', from: :nation
+      click_on 'Search For Members'
+
+      expect(current_path). to eq('/search')
+
+      within(first('.member')) do
+        expect(page).to have_css('.name')
+        expect(page).to have_css('.allies')
+        expect(page).to have_css('.enemies')
+        expect(page).to have_css('.affiliations')
+      end
+    end
   end
 end
